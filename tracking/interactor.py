be9fbc101e5535eab92508d5a737446b63c00adc
swaelali <wa2el.ali@gmail.com>
2017-07-17 09:46:34 +0200

    Update interactor.py

--- tracking/interactor.py
+++ tracking/interactor.py
@@ -1,5 +1,5 @@
 from googlemaps import GoogleMaps
-gmaps = GoogleMaps('AIzaSyCWM-KHA1VHucVMuAAR6VdolYR5CVY2khA')
+gmaps = GoogleMaps('<replace_with_your_key>')
 address = 'Constitution Ave NW & 10th St NW, Washington, DC'
 lat, lng = gmaps.address_to_latlng(address)
 print lat, lng
